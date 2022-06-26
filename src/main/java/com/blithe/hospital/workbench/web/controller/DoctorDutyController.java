e1b03efbbe68006e7bb5c6b3a99cf5869530c8b5
blithe <2697891299@qq.com>
2022-06-26 14:26:12 +0800

    fix some bug of pageList of schedule index.jsp

--- src/main/java/com/blithe/hospital/workbench/web/controller/DoctorDutyController.java
+++ src/main/java/com/blithe/hospital/workbench/web/controller/DoctorDutyController.java
@@ -25,13 +25,28 @@ public class DoctorDutyController {
 
     @RequestMapping("pageList.do")
     @ResponseBody
-    public PaginationVo<DoctorDuty> getPageList(Integer pageNo, Integer pageSize, String project,
+    public PaginationVo<DoctorDuty> getPageList(Integer pageNo, Integer pageSize, String mainClass,
                                     String doctor, String techRoom) {
         DoctorDuty doctorDuty = new DoctorDuty();
         doctorDuty.setDoctorName(doctor);
         doctorDuty.setTechOfficeName(techRoom);
-        doctorDuty.setMainClassName(project);
+        doctorDuty.setMainClassName(mainClass);
         PaginationVo<DoctorDuty> paginationVo = doctorDutyService.pageList(pageNo,pageSize,doctorDuty);
+        for(DoctorDuty duty:paginationVo.getDataList()){
+            duty.setDutyDate(duty.getDutyDate().substring(0,10));
+            if("0".equals(duty.getDutyType())){
+                duty.setDutyType("临时值班");
+            }else{
+
+            }
+            if("2".equals(duty.getDutyTimeStage())){
+                duty.setDutyTimeStage("全天");
+            }else if("1".equals(duty.getDutyTimeStage())){
+                duty.setDutyTimeStage("下午");
+            }else{
+                duty.setDutyTimeStage("上午");
+            }
+        }
         return paginationVo;
     }
 }
